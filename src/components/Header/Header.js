22cca5d4e21166aad629dfae0a1b46b59aed8a9a
deepak-raj2002 <deepak464gupta@gmail.com>
2023-08-18 18:00:54 +0530

    navigation to home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from '../Assets/logo.png';
+import { Link } from "react-router-dom";
 import './Header.scss';
 const Header=()=>{
     return(
         <div className="header">
         <div className="logo-name">
+        <Link to="/">
         <img src={logo} className="header-logo"/>
+        </Link>
         <h3 className="name">TaskFlow</h3>
         </div>
         {/* <button className="strt-btn">Get Started</button> */}
